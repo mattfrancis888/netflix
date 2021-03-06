64d257be0501680455d5c1a5662e0fd268a632f1
Matthew Francis <mattfrancis888@gmail.com>
2021-03-06 11:24:20 -0500

    Set up folders and files.

---
+++ src/components/Register.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+const Register: React.FC<{}> = (props) => {
+    return (
+        <div>
+            <h1>Hi</h1>{" "}
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/components/Routes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+//import Header from "./Header";
+import Register from "./Register";
+const Routes: React.FC<{}> = () => {
+    return (
+        <React.Fragment>
+            {/* <Header /> */}
+            <Switch>
+                <Route path="/" exact component={Register} />
+            </Switch>
+        </React.Fragment>
+    );
+};
+
+export default Routes;
